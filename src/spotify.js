3737571bd8c608db603861192ee258262f169675
Ayo Abimbola <ayoinspired@gmail.com>
2021-05-13 13:00:46 +0100

    setting up login, authentication, and redirect to localhost.

--- src/spotify.js
+++ src/spotify.js
@@ -13,11 +13,11 @@ const scopes = [
 ];
 
 export const getTokenFromUrl = () => {
- return window.location.hash.substring(1).split('&').reduce((initial, item) ={
-  let parts = item.split('=')
-  initial[parts[0]] = decodeURIComponent(parts[1])
- })
-}
+	// return window.location.hash.substring(1).split('&').reduce((initial, item) ={
+	//  let parts = item.split('=')
+	//  initial[parts[0]] = decodeURIComponent(parts[1])
+	// })
+};
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
 	"%20"
