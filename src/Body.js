207b4d596a78dc70d4e96aa4c490d4b8859e484e
Ayo Abimbola <ayoinspired@gmail.com>
2021-05-22 08:34:11 +0100

    setting up song rows in app body.

--- src/Body.js
+++ src/Body.js
@@ -5,21 +5,18 @@ import Header from "./Header";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import SongRow from "./SongRow";
 
 function Body({ spotify }) {
-	const [{ discover_weekly }, dispatch] = useDataLayerValue();
+	const [{ discover_weekly, user }, dispatch] = useDataLayerValue();
+	console.log(user?.tracks);
 
 	return (
 		<div className="body">
 			<Header spotify={spotify} />
 
 			<div className="body__info">
-				<img
-					// src="https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly.png"
-					src={discover_weekly?.images[0].url}
-					alt=""
-				/>
-
+				<img src={user?.images[0].url} alt="" />
 				<div className="body__infoText">
 					<strong>PLAYLIST</strong>
 					<h2>Discover Weekly</h2>
@@ -32,10 +29,16 @@ function Body({ spotify }) {
 
 			<div className="body__songs">
 				<div className="body__icons">
-					<PlayCircleFilledIcon />
-					<FavoriteIcon />
+					<PlayCircleFilledIcon className="body__shuffle mui__icon" />
+					<FavoriteIcon fontSize="large" className="mui__icon" />
 					<MoreHorizIcon />
 				</div>
+
+				{/* {LIST OF SONGS} */}
+
+				{discover_weekly?.tracks.items.map((item) => (
+					<SongRow track={item.track} />
+				))}
 			</div>
 		</div>
 	);
